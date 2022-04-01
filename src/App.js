95625d1c7bbde5695089202c1573de5dffe4a467
amymaryflores <amyflores125@gmail.com>
2022-04-01 18:31:09 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-
+import React from 'react';
+import Weather from "./Weather"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
+export default function App() {
+  return ( 
+  <div className="App">
+  <div className="container">
+  <Weather defaultCity="Worcester" />
+  <footer>
+  This project was coded by Amy Flores and is {" "}
+  <a href="https://github.com/amymaryflores/react-weather-app"
+  target="_blank"
+   rel="noopener noreferrer"
+  >
+open-source on GitHub
+  </a>{" "}
+  and{" "}
+  <a href="https://dainty-snickerdoodle-fbc3bb.netlify.app"
+  target="_blank"
+   rel="noopener noreferrer"
+   >
+     hosted on Netlify
+   </a>
+  </footer>
+  </div>
+  </div>
   );
 }
-
-export default App;
